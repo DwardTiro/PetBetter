470e299dce3cfd74657d2846170e66d721230e22
DwardTiro <dwrdtiro0@gmail.com>
2017-07-09 10:38:49 +0800

    Update 1 DrawerLayout

--- app/src/main/java/com/example/owner/petbetter/HomeActivity.java
+++ app/src/main/java/com/example/owner/petbetter/HomeActivity.java
@@ -1,20 +1,41 @@
 package com.example.owner.petbetter;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Button btnMaps;
+    private DrawerLayout nDrawerLayout;
+    private ActionBarDrawerToggle nToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         System.out.println("Im a pet owner.");
         btnMaps = (Button) findViewById(R.id.btnMaps);
+
+        nDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        nToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
+
+        nDrawerLayout.addDrawerListener(nToggle);
+        nToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(nToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void toMaps(View v){
