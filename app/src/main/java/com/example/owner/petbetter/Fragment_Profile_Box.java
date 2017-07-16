5d890ba4b977b32fd12a6b4fbd08e0ae5c2f04c2
gabogabo <sisayan_kristian@yahoo.com>
2017-07-16 10:36:19 +0800

    UI - Home Screen Content

--- app/src/main/java/com/example/owner/petbetter/Fragment_Profile_Box.java
+++ app/src/main/java/com/example/owner/petbetter/Fragment_Profile_Box.java
@@ -1,5 +1,9 @@
 package com.example.owner.petbetter;
 
+/**
+ * Created by Kristian on 7/16/2017.
+ */
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,17 +13,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-/**
- * Created by owner on 11/7/2017.
- */
 
-public class Tab1Fragment extends Fragment {
+public class Fragment_Profile_Box extends Fragment{
 
-
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.home_tab1, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile__box, container, false);
+
 
         return view;
     }
