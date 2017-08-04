9c3bd884155570709051ba6e923fc260b34d483d
gabogabo <sisayan_kristian@yahoo.com>
2017-08-04 12:56:50 +0800

    UI Update - Fragment Vet Listings

---
+++ app/src/main/java/com/example/owner/petbetter/fragments/fragment_vet_listing.java
@@ -0,0 +1,20 @@
+package com.example.owner.petbetter.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.owner.petbetter.R;
+
+/**
+ * Created by Kristian on 8/4/2017.
+ */
+
+public class fragment_vet_listing extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        return inflater.inflate(R.layout.fragment_vet_listings,container, false);
+    }
+}
