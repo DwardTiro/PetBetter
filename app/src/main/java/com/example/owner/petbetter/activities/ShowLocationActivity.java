fb83a14715c1504641e2abbe52c6bbe5a4b767bc
gabogabo <sisayan_kristian@yahoo.com>
2018-03-04 00:48:07 +0800

    Cleaner UI for post replies and comment thread

---
+++ app/src/main/java/com/example/owner/petbetter/activities/ShowLocationActivity.java
@@ -0,0 +1,60 @@
+package com.example.owner.petbetter.activities;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.owner.petbetter.R;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+/**
+ * Created by Kristian on 3/3/2018.
+ */
+
+public class ShowLocationActivity extends FragmentActivity
+        implements OnMapReadyCallback,
+        LocationListener,
+        GoogleMap.OnMyLocationButtonClickListener
+{
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_show_location);
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
