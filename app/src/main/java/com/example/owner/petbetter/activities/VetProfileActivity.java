a1ae40b128b754c80d7f25017c6c6d578a5a0014
gabogabo <sisayan_kristian@yahoo.com>
2018-03-11 01:01:59 +0800

    Changes to UI - Facility Profile and Vet Profile

--- app/src/main/java/com/example/owner/petbetter/activities/VetProfileActivity.java
+++ app/src/main/java/com/example/owner/petbetter/activities/VetProfileActivity.java
@@ -108,6 +108,7 @@ public class VetProfileActivity extends AppCompatActivity {
 
             }
         });
+
         messageVetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
